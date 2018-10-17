58fb02fd3a9a012f4544b49876c6e37d073b71c4
arronbillwsy <arronbillwsy@163.com>
2018-10-17 09:17:37 -0400

    1017

---
+++ src/main/scala/getKeySentences.scala
@@ -0,0 +1,82 @@
+import breeze.linalg.DenseMatrix
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+
+object getKeySentences {
+
+  val conf = new SparkConf().setAppName("wordcount").setMaster("local");
+  val sc = new SparkContext(conf)
+
+  def main(args: Array[String]): Unit = {
+
+    val i = 0;
+    for( i <- 1 to 8) {
+      val wordFile = s"file:///C:/Users/31476/Desktop/543/bytecup2018/bytecup.corpus.train.${i}.txt"
+      val data = readContent(wordFile)
+      val raw = data.select("content").rdd.collect()
+      for(i <- 0 until raw.length){
+        var text = raw(i).toString()
+        text = text.substring(1,text.length()-1)
+        val key_sentences = textRank(text)
+        data.col("content")[i] = key_sentences
+        print(1)
+      }
+    }
+  }
+
+
+
+  def readContent(path : String) ={
+    val spark = SparkSession.builder().master("local").appName("readWords")
+      .config("spark.some.config.option", "some-value").getOrCreate()
+    import spark.implicits._
+    val stringFrame = spark.read.text(path).as[String]
+    val jsonFrame = spark.read.json(stringFrame)
+    val data = jsonFrame.select("id","title", "content").filter("content != ''")
+    data
+  }
+
+  def textRank(content: String): Array[String] ={
+    val threshhold = 0
+    val top_k = 10
+    val separator = Array('.','!','?',';','-')
+    val sentences = content.split(separator).distinct
+    val i=0
+    var vertices: Array[(Long,String)]=Array()
+    var edges: Array[Edge[Double]] = Array()
+    for (i <- 0 until sentences.length){
+      val s1 = sentences(i)
+      vertices :+= (i.toLong,s1)
+      val j=0
+      for (j <- 0 until sentences.length){
+        if (i!=j){
+          val s2 = sentences(j)
+          val sim = cal_sen_similarity(s1,s2)
+          if (sim>threshhold){
+            edges :+= Edge(i.toLong,j.toLong,sim)
+            edges :+= Edge(j.toLong,i.toLong,sim)
+          }
+        }
+      }
+    }
+    val vRDD= sc.parallelize(vertices)
+    val eRDD= sc.parallelize(edges)
+    val graph = Graph(vRDD,eRDD)
+    val ranks = graph.pageRank(0.0001).vertices
+    val scores = vRDD.join(ranks)
+    val sorted_scores = scores.sortBy(_._2._2, false)
+    val key_sentences = sorted_scores.take(10).map(_._2._1).take(top_k)
+    key_sentences
+  }
+
+  def cal_sen_similarity(s1 : String,s2 : String): Double ={
+    val words1 = sc.parallelize(s1.split(" "))
+    val words2 = sc.parallelize(s2.split(" "))
+    val intersection = words1.intersection(words2)
+    val sim = intersection.count()/(math.log(words1.count())*math.log(words2.count()))
+    sim
+  }
+
+}
--- src/main/scala/wordcount.scala
+++ src/main/scala/wordcount.scala
@@ -1,16 +1,50 @@
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 
 object wordcount {
 
   def main(args: Array[String]): Unit = {
+    val i = 0
+    var filepath = s"/C:/Users/31476/Desktop/543/bytecup2018/processed_train.${i}.txt"
+    val data = readWords(filepath)
+    val raw = data.select("words").rdd.collect()
+    for(j <- 0 until raw.length){
+      val text = raw(j).toString()
+
+    }
+
+
 
     val conf = new SparkConf().setAppName("wordcount").setMaster("local");
     val sc = new SparkContext(conf)
-    val wordFile = "file:///C:/Users/31476/Desktop/543/bytecup2018/bytecup.corpus.train.0.txt"
 
-    val input = sc.textFile(wordFile, 2).cache()
-    val lines = input.flatMap(line=>line.split(" "))
-    val count = lines.map(word => (word,1)).reduceByKey{case (x,y)=>x+y}
-    val output = count.saveAsTextFile("/C:/Users/31476/Desktop/543/bytecup2018/res.txt")
+
+
+
+
+
+
+    for( i <- 1 to 8){
+      val wordFile = s"file:///C:/Users/31476/Desktop/543/bytecup2018/bytecup.corpus.train.${i}.txt"
+      val input = sc.textFile(wordFile, 2).cache()
+      val lines = input.flatMap(line=>line.split(" "))
+//      count = lines.map(word => (word,1)).reduceByKey{case (x,y)=>x+y}.union(count)
+      println(i)
+    }
+//    count.reduceByKey{case (x,y)=>x+y}
+//    val output = count.saveAsTextFile("/C:/Users/31476/Desktop/543/bytecup2018/res")
   }
+
+  def readWords(path : String) ={
+    val spark = SparkSession.builder().master("local").appName("readWords")
+      .config("spark.some.config.option", "some-value").getOrCreate()
+
+    import spark.implicits._
+    val stringFrame = spark.read.text(path).as[String]
+    val jsonFrame = spark.read.json(stringFrame)
+    val data = jsonFrame.select("id","words", "title","content").filter("content != ''")
+    data
+  }
+
+
 }
